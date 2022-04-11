36fc728c00864029e5eae5a1124dd59f6d810009
danisole <danisole82@gmail.com>
2022-04-10 21:24:24 -0300

    accesibilidad

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const ullNav = document.querySelector(".ullNav");
 
 navToggle.addEventListener("click", ()=>{
     ullNav.classList.toggle("ullNav_visible");
+
+    if (ullNav.classList.contains("ullNav_visible")){
+        navToggle.setAttribute("arial-label", "cerrar menú");
+    }else{
+        navToggle.setAttribute("arial-label", "Abrir menú")
+    }
 });
